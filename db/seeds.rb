69273e7ef2547468f19d22ce1fce9d37077663c4
more-like-a-puma <jessicamaycreative@gmail.com>
2016-06-01 16:46:00 +1000

    FOnTs

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,4 @@ morelikeapuma = User.create :username => 'more_like_a_puma', :email => 'jwannabe
 
 Destination.destroy_all
 bondibeach = Destination.create :address => 'Bondi Beach, Sydney, Australia', :country => 'Australia', :terrain => 'seascape', :description => 'World famous surf beach in the heart of Sydney, Australia.', :title => 'Bondi Beach'
+bluemountains = Destination.create :address => 'Leura, Blue Mountains', :country => 'Australia', :terrain => 'mountain || forest', :description => 'mountain || forest', :title => 'Blue Mountains'
