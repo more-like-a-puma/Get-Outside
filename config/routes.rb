213a82049f0fb1b1b73ddd927ab1bd109300175c
more-like-a-puma <jessicamaycreative@gmail.com>
2016-06-02 18:09:04 +1000

    All tables styling done, now to join them together

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'activities/index'
-
-  get 'activities/new'
-
-  get 'activities/show'
-
-  get 'activities/edit'
-
-  get 'activities/_form'
-
   root "users#home"
   get 'session/new'
 
   resources :users
   resources :destinations
+  resources :activities
 
   get "/login" => 'session#new', :as => 'login'
   post '/login' => 'session#create'
